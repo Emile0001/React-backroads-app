3ddab6b4001b7341b20b07fc99a63ffb86d64265
Emile0001 <boteseh@gmail.com>
2024-09-22 16:46:11 +0200

    About Us

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { Nav } from "./components/Nav/Nav";
 import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
 
 function App() {
     return (
         <>
             <Nav></Nav>
             <Hero></Hero>
+            <About></About>
         </>
     );
 }
