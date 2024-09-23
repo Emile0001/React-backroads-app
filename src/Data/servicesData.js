07af9ebbb511a325e00a22b09cfb6177bca7f600
Emile0001 <boteseh@gmail.com>
2024-09-23 12:08:40 +0200

    Dynamic Service Component

---
+++ src/Data/servicesData.js
@@ -0,0 +1,20 @@
+export const servicesData = [
+    {
+        id: 1,
+        icon: "fas fa-wallet fa-fw",
+        title: "saving money",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    },
+    {
+        id: 2,
+        icon: "fas fa-tree fa-fw",
+        title: "endless hikin",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    },
+    {
+        id: 3,
+        icon: "fas fa-socks fa-fw",
+        title: "amazing comfort",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    },
+];
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,3 +1,4 @@
+import { servicesData } from "../../Data/servicesData";
 import { Title } from "../Title/Title";
 import "./Services.css";
 
@@ -6,44 +7,21 @@ export const Services = () => {
         <section className="section services" id="services">
             <Title title="Our" subtitle="Services"></Title>
             <div className="section-center services-center">
-                <article className="service">
-                    <span className="service-icon">
-                        <i className="fas fa-wallet fa-fw"></i>
-                    </span>
-                    <div className="service-info">
-                        <h4 className="service-title">saving money</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon">
-                        <i className="fas fa-tree fa-fw"></i>
-                    </span>
-                    <div className="service-info">
-                        <h4 className="service-title">endless hiking</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon">
-                        <i className="fas fa-socks fa-fw"></i>
-                    </span>
-                    <div className="service-info">
-                        <h4 className="service-title">amazing comfort</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
+                {servicesData.map((service) => {
+                    return (
+                        <article key={service.id} className="service">
+                            <span className="service-icon">
+                                <i className={service.icon}></i>
+                            </span>
+                            <div className="service-info">
+                                <h4 className="service-title">
+                                    {service.title}
+                                </h4>
+                                <p className="service-text">{service.text}</p>
+                            </div>
+                        </article>
+                    );
+                })}
             </div>
         </section>
     );
