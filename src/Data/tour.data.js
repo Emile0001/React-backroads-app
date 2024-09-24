967a3aa1ec879cd370d80d47449fe8247675f5c0
Emile0001 <boteseh@gmail.com>
2024-09-24 14:52:09 +0200

    refactor Tour Component

---
+++ src/Data/tour.data.js
@@ -0,0 +1,51 @@
+import tour1Img from "./../images/tour-1.jpeg";
+import tour2Img from "./../images/tour-2.jpeg";
+import tour3Img from "./../images/tour-3.jpeg";
+import tour4Img from "./../images/tour-4.jpeg";
+
+export const tourData = [
+    {
+        id: 1,
+        img: tour1Img,
+        date: "august 26th, 2020",
+        title: "Tibet Adventure",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        icon: "fas fa-map",
+        destination: "china",
+        duration: "6 days",
+        price: "from $2100",
+    },
+    {
+        id: 2,
+        img: tour2Img,
+        date: "october 1th, 2020",
+        title: "best of java",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        icon: "fas fa-map",
+        destination: "indonesia",
+        duration: "11 days",
+        price: "from $1400",
+    },
+    {
+        id: 3,
+        img: tour3Img,
+        date: "september 15th, 2020",
+        title: "explore hong kong",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        icon: "fas fa-map",
+        destination: "hong kong",
+        duration: "8 days",
+        price: "from $5000",
+    },
+    {
+        id: 4,
+        img: tour4Img,
+        date: "december 5th, 2019",
+        title: "kenya highlights",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        icon: "fas fa-map",
+        destination: "kenya",
+        duration: "20 days",
+        price: "from $3300",
+    },
+];
---
+++ src/components/Tours/ToursCardComponent.js
@@ -0,0 +1,34 @@
+import { tourData } from "../../Data/tour.data";
+
+export const ToursCardComponent = () => {
+    return (
+        <>
+            {tourData.map((tours) => {
+                return (
+                    <article key={tours.id} className="tour-card">
+                        <div className="tour-img-container">
+                            <img src={tours.img} className="tour-img" alt="" />
+                            <p className="tour-date">{tours.date}</p>
+                        </div>
+                        <div className="tour-info">
+                            <div className="tour-title">
+                                <h4>{tours.title}</h4>
+                            </div>
+                            <p>{tours.text}</p>
+                            <div className="tour-footer">
+                                <p>
+                                    <span>
+                                        <i className={tours.icon}></i>
+                                    </span>
+                                    {tours.destination}
+                                </p>
+                                <p>{tours.duration}</p>
+                                <p>{tours.price}</p>
+                            </div>
+                        </div>
+                    </article>
+                );
+            })}
+        </>
+    );
+};
